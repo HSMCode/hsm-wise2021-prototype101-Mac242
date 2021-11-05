c09d942bf62f5c809b11ed3122ed901ff1e37307
Mac242 <72981027+Mac242@users.noreply.github.com>
2021-11-05 15:05:29 +0100

    create script DebugTest

--- Assets/Scripts/DebugTest.cs
+++ Assets/Scripts/DebugTest.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstScript : MonoBehaviour
+public class DebugTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hallo");
     }
 
     // Update is called once per frame
