7611ba5fd33fa37abc4aedbd5e4cb44c7a92c029
Anna Moritz <72981027+Mac242@users.noreply.github.com>
2021-11-17 17:32:14 +0100

    update RandomDice20
    add winning numbers

--- Assets/Scripts/RandomDice20.cs
+++ Assets/Scripts/RandomDice20.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class RandomDice20 : MonoBehaviour
 {
     public int currentDiceNumber;
+    public string winText = "-You WIN!";
+    public string loseText = "-No Luck, try again!";
     
     // Start is called before the first frame update
     void Start()
@@ -17,8 +20,22 @@ public class RandomDice20 : MonoBehaviour
     {
         if (Input.anyKeyDown)
         {
+            //setting random range for dice
             currentDiceNumber = Random.Range(1, 21);
-            Debug.Log(currentDiceNumber);
+            //Debug.Log(currentDiceNumber);
+            
+            //checking for winning numbers
+            if (currentDiceNumber == 7 || currentDiceNumber ==13 || currentDiceNumber ==1)
+            {
+                print(currentDiceNumber + winText);
+            }
+            else
+            {
+                print(currentDiceNumber + loseText);
+            }
         }
+
+        
+            
     }
 }
